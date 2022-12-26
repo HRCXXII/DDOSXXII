1abc8e4b288d331e19aa19c45565b353063ad4f9
MR.HRCXXII <betazona1984@gmail.com>
2022-12-26 23:50:00 +0900

    Update and rename ddosroniyt.sh to DDOS-HRCXXII.sh

--- DDOS-HRCXXII.sh
+++ DDOS-HRCXXII.sh
@@ -1,25 +1,25 @@
 clear
-figlet RoniYT |lolcat
+figlet HRCXXII |lolcat
 
 echo "\33[32;1m[1] DDOS ATTACK"
 echo "\33[32;1m[2] Keluar"
 echo
 echo "\33[31;1mIngat !! Saya Tidak Bertanggung Jawab"
 
-read -p "Masukan Nomor Pilihan Anda:" roni
+read -p "Masukan Nomor Pilihan Anda:" HRCXXII
 
 clear
-case $roni in
+case $HRCXXII in
 1)
 figlet DDOS |lolcat
-python2 attack.py
+python2 ddos.py
 ;;
 2)
 echo "\33[31;1mKeluar Berhasil"
-echo "\33[31;1mJangan Lupa Subscribe RoniYT"
+echo "\33[31;1mJangan Lupa Subscribe HRCXXII"
 ;;
 *)
 echo "Maaf Nomor Yang Anda Ketik Salah"
-sh ddosroniyt.sh
+sh DDOS-HRCXXII.sh
 ;;
 esac
